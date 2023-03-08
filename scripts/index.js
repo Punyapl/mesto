947184d15ad9c229b1aae7861ceeb83a7f9410c5
punyapl <vasabi.light2005@gmail.com>
2023-03-08 20:05:26 +0700

    feat: добавлена функция добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,120 @@
-// Находим форму в DOM
-const formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
+const editFormElement = document.querySelector('.popup__form-edit');
+const addFormElement = document.querySelector('.popup__form-add');
 const nameInput = document.querySelector('.popup__input_name');
 const jobInput = document.querySelector('.popup__input_job');
 const nameOutput = document.querySelector('.profile__name');
 const jobOutput = document.querySelector('.profile__caption');
+const nameInputCard = document.querySelector('.popup__input_card-name');
+const linkInputCard = document.querySelector('.popup__input_card-link');
+
 const profileSaveBtn = document.querySelector('.popup__savebut_svbtn');
 const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup-edit');
+const popupAdd = document.querySelector('.popup-add');
 
-const profileEditExit = document.querySelector(".popup__closebtn")
-const profileEditBtn = document.querySelector(".profile__edit")
+const profileEditExit = document.querySelector(".popup__closebtn-edit");
+const profileEditBtn = document.querySelector(".profile__edit");
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-console.log("done!")
-function handleFormSubmit (evt) {
+const cardAddBtn = document.querySelector(".profile__add");
+const cardAddExit = document.querySelector(".popup__closebtn-add");
+
+const cardTemplate = document.querySelector("#card").content;
+const cardElement = cardTemplate.querySelector(".elements__element");
+const cardsContainer = document.querySelector('.elements');
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+function EditPopupClose(){
+    popupEdit.classList.remove('popup_opened');
+}
+
+function EditPopupOpen(){
+    popupEdit.classList.add('popup_opened');
+}
+
+function AddPopupClose(){
+    popupAdd.classList.remove('popup_opened');
+}
+
+function AddPopupOpen(){
+    popupAdd.classList.add('popup_opened');
+}
+
+function handleProfileFormSubmit (evt) {
     evt.preventDefault(); 
     nameOutput.textContent = nameInput.value;
     jobOutput.textContent = jobInput.value;
-    PopupClose()
+    EditPopupClose()
 }
 
-function PopupClose(){
-    popup.classList.remove('popup_opened');
+function handleAddFormSubmit(evt){
+    evt.preventDefault(); 
+    const cardName = nameInputCard.value;
+    const cardLink = linkInputCard.value;
+    createNewCard(cardName, cardLink);
+    renderCard(cardName, cardLink)
+    evt.target.reset();
+    AddPopupClose()
 }
 
-function PopupOpen(){
-    popup.classList.add('popup_opened');
+function createNewCard(name,link){
+    const cardClone = cardElement.cloneNode(true);
+    const cardPic = cardClone.querySelector(".elements__image");
+    const cardText = cardClone.querySelector(".elements__text");
+    const deleteBtn = cardClone.querySelector(".elements__dlt-btn");
+    const likeBtn = cardClone.querySelector(".elements__likebut");
+
+    deleteBtn.addEventListener("click", () => cardClone.remove());
+    likeBtn.addEventListener("click", () =>
+    likeBtn.classList.toggle("elements__likebut_active")
+  );
+
+    cardPic.src = link;
+    cardPic.alt = name;
+    cardText.textContent = name;
+
+    return cardClone;
 }
 
-// function likeBtnTggl(){
-    
-// }
+function renderCard(name,link){
+    const newCard = createNewCard(name,link);
+    cardsContainer.prepend(newCard);
+}
 
-profileEditExit.addEventListener('click', PopupClose);
-profileEditBtn.addEventListener('click', PopupOpen);
+initialCards.forEach((card) => {
+    renderCard(card.name, card.link);
+  });
+
+profileEditExit.addEventListener('click', EditPopupClose);
+profileEditBtn.addEventListener('click', EditPopupOpen);
+cardAddBtn.addEventListener('click', AddPopupOpen);
+cardAddExit.addEventListener('click', AddPopupClose);
 
 
-formElement.addEventListener('submit', handleFormSubmit); 
+editFormElement.addEventListener('submit', handleProfileFormSubmit); 
+addFormElement.addEventListener("submit", handleAddFormSubmit);
