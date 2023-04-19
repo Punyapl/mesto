8bebaea4a8fceef20a2fc59f48673cf98fb3387c
punyapl <vasabi.light2005@gmail.com>
2023-04-19 23:43:35 +0700

    dev: Вынос инициализации карточек в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import FormValidator from "./FormValidator.js";
 import Card from "./Card.js";
+import initialCards from "./initialCards.js";
 
 const editFormElement = document.querySelector('.popup__form-edit');
 const addFormElement = document.querySelector('.popup__form-add');
@@ -38,33 +39,6 @@ const validationList = ({
   inputErrorClass: "popup__input_error",
   errorClass: "popup__error_active"
 });
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
